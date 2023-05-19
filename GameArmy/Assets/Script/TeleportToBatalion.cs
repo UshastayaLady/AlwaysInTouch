6940f53f39219764727dd6cf1c1b5ee196444159
TaabuLaR <trubachev.m@mail.ru>
2023-05-19 11:56:49 +0300

    make default teleport

---
+++ GameArmy/Assets/Script/TeleportToBatalion.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleportToBatalion : MonoBehaviour
+{
+    public GameObject Marker;
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
